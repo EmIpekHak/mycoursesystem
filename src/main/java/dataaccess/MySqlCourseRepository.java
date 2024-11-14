0cfe98163894dc8eaecc6c916f9d4572566dbcfd
Emin Musa Ipek <eminmusaipek@gmail.com>
2024-11-14 15:57:48 +0100

    Bis Video 8 programmiert

--- src/main/java/dataaccess/MySqlCourseRepository.java
+++ src/main/java/dataaccess/MySqlCourseRepository.java
@@ -1,4 +1,81 @@
 package dataaccess;
 
-public class MySqlCourseRepository {
+import domain.Course;
+import domain.CourseType;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class MySqlCourseRepository implements MyCourseRepository{
+
+    private Connection con;
+
+    public MySqlCourseRepository(Connection con) throws SQLException, ClassNotFoundException {
+        this.con = MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/kurssystem", "root", "");
+    }
+
+    @Override
+    public List<Course> findAllCoursesByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findAllCoursesByCourseType(CourseType courseType) {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findAllCoursesByBeginDate(Date beginDate) {
+        String sql = "SELECT * FROM `courses`";
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+        } catch (SQLException e) {
+            throw new DatabaseException("Database error occured");
+        }
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findAllCoursesByEndDate(Date endDate) {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findAllCoursesByDescription(String description) {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findAllRunningCourses() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Course> insert(Course entity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Course> update(Course entity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Course> getById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Course> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
